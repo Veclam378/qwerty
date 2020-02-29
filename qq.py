eea3b2b73d002fd8a5caa2e4683377e0b6857054
Veclam378 <61622307+Veclam378@users.noreply.github.com>
2020-02-29 14:40:55 +0200

    Add files via upload

---
+++ qq.py
@@ -0,0 +1,46 @@
+# counter = 0
+# def myFunc():
+#    global counter
+#    counter += 1
+#    label.config(text = counter)
+# from tkinter import *
+# window = Tk()
+# a = 0
+# window.title("lol")
+# window.minsize(300,500)
+# window.resizable(True,False)
+# label = Label(window, text="hello", font="Arial 30", fg="red")
+# button = Button(window, text="Click me", bg="yellow", fg="blue", font="Arial 30", width=10, height=5)
+# entry = Entry(window)
+# entry.pack()
+# button.pack()
+# label.pack(padx=20, pady=50)
+# window.mainloop()
+# from tkinter import *
+# # root = Tk()
+# # root.geometry("500x500")
+# # root.resizable(True, False)
+# # f_top = Frame(root)
+# # f_bot = Frame(root)
+# #
+# # text = Label(f_top, bg="red", width="7", height="4")
+# # f_top.pack()
+# # f_bot.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
